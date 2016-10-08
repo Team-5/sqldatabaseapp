2877965e923ede35f65b5334a8942d270c5c6aed
Jason Whiting <Jason Whiting>
2016-10-08 11:23:03 -0400

    Added minor changes to files and imports to CustomerApp

--- src/viewui/CustomerApp.java
+++ src/viewui/CustomerApp.java
@@ -1,5 +1,19 @@
 package viewui;
 
+import util.Validator;
+import java.util.Scanner;
+import model.Customer;
+import model.ICustomerDAO;
+import model.datastore.CustomerDAO;
+
+/**
+ * CustomerApp is the starting point for running this console-oriented
+ * menu-driven customer management program. This program demonstrates
+ * only a MySQL based solution.
+ * 
+ * @author Jason Whiting
+ * @version 2016-10-08
+ */
 public class CustomerApp {
 
 }
